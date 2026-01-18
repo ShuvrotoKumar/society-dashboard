80793571f4b62d64eaf7c7392ffaa5296831f53d
ShuvrotoKumar <sdks6409@gmail.com>
2026-01-19 02:01:42 +0600

    logo  resized

--- src/shared/Sidebar/Sidebar.jsx
+++ src/shared/Sidebar/Sidebar.jsx
@@ -41,7 +41,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
 
       {/* Logo */}
       <div className="flex justify-center items-center gap-2 px-5 mt-20">
-        <img src="/logo.png" className="w-[100px] h-[100px]" alt="User Avatar" />
+        <img src="/logo.png" className="w-[70px] h-[70px]" alt="User Avatar" />
       </div>
 
       {/* Sidebar Menu */}
