4594aa549276ac8657b842db6b14f40a52ec09d7
ShuvrotoKumar <sdks6409@gmail.com>
2026-02-16 06:14:25 +0600

    resolved connecting issues

---
+++ src/redux/api/adminApi.js
@@ -0,0 +1,31 @@
+import { baseApi } from "./baseApi";
+
+const adminApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAdmin: builder.query({
+      query: () => ({
+        url: "/admin/all-admins",
+        method: "GET",
+      }),
+      providesTags: ["admin"],
+    }),
+    createAdmin: builder.mutation({
+      query: ({ requestData }) => ({
+        url: "/legal-docs/about-us",
+        method: "POST",
+        body: requestData,
+      }),
+      invalidatesTags: ["about"],
+    }),
+    updateAdmin: builder.mutation({
+      query: ({ requestData }) => ({
+        url: "/legal-docs/about-us",
+        method: "PATCH",
+        body: requestData,
+      }),
+      invalidatesTags: ["about"],
+    }),
+  }),
+});
+
+export const { useGetAdminQuery, useCreateAdminMutation, useUpdateAdminMutation } = adminApi;
